094208a3f6619e28c7f6d8465dd9346fadc7a3a1
FranciscoRMendes <mendes.franciscoromaldo@gmail.com>
2025-02-07 12:03:50 -0600

    main_sac.py refactor

--- main_sac.py
+++ main_sac.py
@@ -6,8 +6,8 @@ from sac_utils.utils import plot_learning_curve
 
 if __name__ == '__main__':
     env = gym.make('InvertedPendulumBulletEnv-v0')
-    agent = SoftActorCritic(input_dims=env.observation_space.shape, env=env,
-                            n_actions=env.action_space.shape[0])
+    soft_actor_critic = SoftActorCritic(input_dims=env.observation_space.shape, env=env,
+                                        n_actions=env.action_space.shape[0])
     n_games = 250
     # uncomment this line and do a mkdir tmp && mkdir video if you want to
     # record video of the agent playing the game.
@@ -17,36 +17,36 @@ if __name__ == '__main__':
     figure_file = 'plots/' + filename
 
     best_score = env.reward_range[0]
-    score_history = []
+    R = [] # total rewards
     load_checkpoint = False
 
     if load_checkpoint:
-        agent.load_models()
+        soft_actor_critic.load_models()
         env.render(mode='human')
 
     for i in range(n_games):
-        observation = env.reset()
+        s_t = env.reset()
         done = False
-        score = 0
+        R_T = 0
         while not done:
-            action = agent.choose_action(observation)
-            observation_, reward, done, info = env.step(action)
-            score += reward
-            agent.remember(observation, action, reward, observation_, done)
+            action = soft_actor_critic.choose_action(s_t)
+            s_t_plus_1, r_t, done, info = env.step(action)
+            R_T += r_t
+            soft_actor_critic.remember(s_t, action, r_t, s_t_plus_1, done)
             if not load_checkpoint:
-                agent.learn()
-            observation = observation_
-        score_history.append(score)
-        avg_score = np.mean(score_history[-100:])
+                soft_actor_critic.learn()
+            s_t = s_t_plus_1
+        R.append(R_T)
+        avg_score = np.mean(R[-100:])
 
         if avg_score > best_score:
             best_score = avg_score
             if not load_checkpoint:
-                agent.save_models()
+                soft_actor_critic.save_models()
 
-        print('episode ', i, 'score %.1f' % score, 'avg_score %.1f' % avg_score)
+        print('episode ', i, 'score %.1f' % R_T, 'avg_score %.1f' % avg_score)
 
     if not load_checkpoint:
         x = [i+1 for i in range(n_games)]
-        plot_learning_curve(x, score_history, figure_file)
+        plot_learning_curve(x, R, figure_file)
 
